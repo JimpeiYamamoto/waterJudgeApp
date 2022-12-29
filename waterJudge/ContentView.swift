2fae511c4dda3053195f4e9f95abbfde100b2de6
Jimpei Yamamoto <zinpei104@gmail.com>
2022-12-29 22:59:21 +0900

    Initial Commit

---
+++ waterJudge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  waterJudge
+//
+//  Created by 水代謝システム工学研究室 on 2022/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ waterJudge/waterJudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterJudgeApp.swift
+//  waterJudge
+//
+//  Created by 水代謝システム工学研究室 on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct waterJudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
